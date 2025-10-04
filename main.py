3a78dc1c1bbd7c9d202b3bd1e592448d934bfa0f
Chanok Pathompatai <cpathompatai@gmail.com>
2025-10-04 19:32:18 +0700

    add inher

--- main.py
+++ main.py
@@ -1,17 +1,78 @@
-line = input().split()
-size, number, angle = [int(e) for e in line[:-1]]
-hp = float(line[-1])
+# Inheritance
+# สืบทอด
 
-print(
-    size,
-    number,
-    angle,
-    hp,
-)
-blade = Blade(
-    size=size,
-    number=number,
-    angle=angle,
-    hp=hp,
-)
-blade.printInfo()
+# sub class, child class
+# super class, parent class
+# super() # get parent class
+# override
+
+class Character:
+    def __init__(self, str, dex, int):
+        self.str = str
+        self.dex = dex
+        self.int = int
+
+    def attack(self):
+        return sum([self.str, self.dex, self.int]) / 3
+
+    def __str__(self):
+        return "CHARACTER"
+
+
+class Swordman(Character):
+    def __init__(self):
+        # Character.__init__(self, 20, 10, 0)
+        super().__init__(20, 10, 0)
+        self.give = 7
+
+    # Override Method
+    def attack(self):
+        return (self.str * 2) + self.give
+
+    def __str__(self):
+        return "Swordman"
+
+
+class Archer(Character):
+    def __init__(self):
+        super().__init__(10, 20, 0)
+
+    def attack(self):
+        return 2 * super().attack()
+
+    def __str__(self):
+        return "Archer"
+
+
+class Wizard(Character):
+    def __init__(self):
+        super().__init__(5, 5, 25)
+
+    def __str__(self):
+        return "Wizard"
+
+
+class Knight(Swordman):
+    def __init__(self):
+        super().__init__()
+        self.give2 = 100
+
+    def attack(self):
+        # return Character.attack(self)
+        return super(Swordman, self).attack()
+
+swordman = Swordman()
+archer = Archer()
+wizard = Wizard()
+knight = Knight()
+character = Character(1, 1, 1)
+
+character_list = [
+    swordman,
+    archer,
+    wizard,
+    character,
+    knight,
+]
+for hero in character_list:
+    print(hero, hero.attack())
---
+++ main2.py
@@ -0,0 +1,24 @@
+# n
+# n m
+
+for i in range(5):
+    # ["n"]
+    # ["n", "m"]
+    line = input().split()
+
+    if line[0] == "1":
+        pass
+
+    elif line[0] == "2":
+        pass
+
+    elif line[0] == "3":
+        pass
+
+    elif line[0] == "4":
+        m = int(line[1])
+        pass
+
+    elif line[0] == "5":
+        m = int(line[1])
+        pass
